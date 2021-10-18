149a2b04177401c3ffb654379cd3d9898a8a6740
khalid <khalid.Ali.med.s@tuwaiq.edu.sa>
2021-10-18 09:25:35 +0300

    Initial Commit

---
+++ aloooo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aloooo
+//
+//  Created by khalid ali on 12/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
