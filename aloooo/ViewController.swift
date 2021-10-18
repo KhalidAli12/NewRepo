e2fc99d75a2e2f5d34c3fe3988a7fdb48b12dc09
Khalid Alshammeri <khalid.Ali.med.s@tuwaiq.edu.sa>
2021-10-18 10:18:08 +0300

    add commit

--- aloooo/ViewController.swift
+++ aloooo/ViewController.swift
@@ -7,7 +7,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-//    var array = ["Khalid" ,"Ali"]
+//   var array = ["Khalid" ,"Ali"]
+    
+    var mycoffee = "Hot"
 
     override func viewDidLoad() {
         super.viewDidLoad()
